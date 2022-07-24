fdbf92178419ce4cf82144592a3cb591e7a47516
Carlos Leonidas Alcala P <usrdes10@syspotec.com>
2022-07-24 14:00:26 -0500

    CRUD Profesión, formulario reactivo para crear y editar profesión

--- src/app/pages/admin/parametros/profesiones/create-profesiones/create-profesiones.component.ts
+++ src/app/pages/admin/parametros/profesiones/create-profesiones/create-profesiones.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { controllers } from 'chart.js';
+import { IProfesion } from 'src/app/models/profesiones.model';
 
 @Component({
   selector: 'app-create-profesiones',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProfesionesComponent implements OnInit {
 
-  constructor() { }
+  //#region  VARIABLES
+  form : FormGroup;
+
+  //variables del modal
+  action : string;
+  profesion: IProfesion;
+  //#endregion
+
+  //#region CONSTRUCTOR
+  constructor() {
+    this.form = new FormGroup({
+      description : new FormControl('',[Validators.required,Validators.min(4),Validators.max(50)]),
+      estado : new FormControl(true,[Validators.required])
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
+  ngAfterViewInit(){
+    console.log(this.action);
+    console.log(this.profesion);
+    switch (this.action){
+      case "new":
+        break;
+      case "edit":
+        this.setFormulario();
+        break;
+    }
+  }
+
+  //#endregion
+
+
+  //#region FORMULARIO
+  // Asignación de un campo a un formulario
+  private setFormulario() :void {
+    this.form.patchValue({
+      description : 'carlos andres',
+      estado : false
+    })
+  }
+
+
+  //#endregion
 
 }
