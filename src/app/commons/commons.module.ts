63d4f6fd9274e9411521e645a0c8ad2e835febd7
anRoswell <40361918+anRoswell@users.noreply.github.com>
2022-08-01 20:46:16 -0500

    Se arregla el menu

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CommonsRoutingModule } from './commons-routing.module';
 import { LoadingComponent } from './loading/loading.component';
 import { MessageComponent } from './message/message.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    LoadingComponent,
-    MessageComponent,
-    NavbarComponent,
-    FooterComponent,
-  ],
+  declarations: [LoadingComponent, MessageComponent, FooterComponent],
   imports: [CommonModule, CommonsRoutingModule],
 })
 export class CommonsModule {}
