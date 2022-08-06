26deb2c5750cdeebdf2d4a206cf144465ff6ebb7
Carlos Leonidas Alcala P <usrdes10@syspotec.com>
2022-08-06 18:26:29 -0500

    Formulario edición de usuario

--- src/app/pages/admin/user/edit/edit.component.ts
+++ src/app/pages/admin/user/edit/edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
+import { StorageService } from '../../../../services/storage.service';
+import {
+  faSave,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+
+  //#region Variables
+  usuario: any;
+
+  //form
+  formData: FormData = new FormData();
+
+  //fontAwesome
+  faSave = faSave;
+
+  //Formulario
+  form: FormGroup;
+
+  //#endregion
+
+  constructor(
+    private serviceStorage : StorageService,
+    private fb: FormBuilder,
+    private apiService: ApiService,
+  ) {
+    this.form = this.fb.group({
+      id: '0',
+      identificationTypeId: [null, Validators.required],
+      profileId: [null, Validators.required],
+      usr_cedula: [null, Validators.required],
+      usr_direccion: [{ value: null }, Validators.required],
+      usr_email: [null, Validators.required],
+      usr_nameComplete: [Validators.required],
+      usr_nroCelular: [null, Validators.required],
+      usr_telefonoFijo: [null, Validators.required],
+      usr_status: [true, Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+    this.usuario = this.serviceStorage.read('_user')
+    console.log (this.usuario.body)
+    this.setDataToForm();
   }
+
+  accionActualizar(){
+    console.log('Hola')
+  }
+
+  onFileSelected(){
+    console.log('Hola')
+  }
+  //#region formularios
+   //destructuring asigment
+   public setDataToForm() {
+    console.log(this.usuario);
+    const { id, identificationTypeId, profileId, usr_cedula, usr_direccion, usr_email, usr_nameComplete, usr_nroCelular, usr_telefonoFijo, usr_status } = this.usuario.body;
+    this.form.patchValue({
+      id: id,
+      identificationTypeId: identificationTypeId,
+      profileId: profileId,
+      usr_cedula: usr_cedula,
+      usr_direccion: usr_direccion,
+      usr_email: usr_email,
+      usr_nameComplete: usr_nameComplete,
+      usr_nroCelular: usr_nroCelular,
+      usr_telefonoFijo: usr_telefonoFijo,
+      usr_status: usr_status,
+    });
+  }
+  //#endregion
 
 }
--- src/app/pages/admin/user/user.module.ts
+++ src/app/pages/admin/user/user.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { EditComponent } from './edit/edit.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [
     EditComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ]
 })
 export class UserModule { }
